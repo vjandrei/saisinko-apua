87a4a18c79f4653c5d9ea31572cb3b43112d5a66
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-22 09:09:25 +0200

    form refactor and image optimize

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -136,19 +136,37 @@
                 <div class="flex flex-column items-center justify-center my-6">
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" name="launguage" value="suomi" />
+                      <input
+                        type="checkbox"
+                        class="form-checkbox"
+                        name="launguage"
+                        value="suomi"
+                        v-model="formData.value"
+                      />
                       <span class="ml-2">Suomi</span>
                     </label>
                   </div>
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" name="launguage" value="svenska" />
+                      <input
+                        type="checkbox"
+                        class="form-checkbox"
+                        name="launguage"
+                        value="svenska"
+                        v-model="formData.value"
+                      />
                       <span class="ml-2">Svenska</span>
                     </label>
                   </div>
                   <div>
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" name="launguage" value="english" />
+                      <input
+                        type="checkbox"
+                        class="form-checkbox"
+                        name="launguage"
+                        value="english"
+                        v-model="formData.value"
+                      />
                       <span class="ml-2">English</span>
                     </label>
                   </div>
