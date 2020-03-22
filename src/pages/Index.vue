a63aeeda13f7c55532a2e9858c071e021d934d60
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-22 08:20:46 +0200

    added Netlify form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout>
     <section>
-      <div class="container mx-auto ">
+      <div class="container mx-auto">
         <nav class="flex items-center justify-between my-6 py-6">
           <div>
-            <a href="">hackthecrisisfinland.com</a>
+            <a href>hackthecrisisfinland.com</a>
           </div>
           <div class="flex">
-            <a href="" class="mr-4">Tilauslomake</a>
-            <a href="">Mistä on kyse?</a>
+            <a href class="mr-4">Tilauslomake</a>
+            <a href>Mistä on kyse?</a>
           </div>
         </nav>
       </div>
@@ -19,14 +19,8 @@
           <div class="grid grid-cols-1 sm:grid-cols-2">
             <div class="flex items-center justify-center">
               <div class="mb-6">
-                <h1
-                  class="font-display font-extrabold text-5xl text-primary mb-4"
-                >
-                  Saisinko apua?
-                </h1>
-                <h2
-                  class="font-display font-normal text-xl text-secondary mb-4"
-                >
+                <h1 class="font-display font-extrabold text-5xl text-primary mb-4">Saisinko apua?</h1>
+                <h2 class="font-display font-normal text-xl text-secondary mb-4">
                   Suojellaksemme riskiryhmiä ja pandemian leviämistä haluemme
                   varmistaa, ettei kenenkään riskiryhmän edustajan tarvitsisi
                   mennä tarvittaville kauppa- ja apteekkireissulle, ja siten
@@ -34,10 +28,8 @@
                   naapurien voimaan.
                 </h2>
                 <button
-                  class="w-full sm:w-auto font-display font-semibold text-lg bg-primary text-white rounded-full py-2 px-6 "
-                >
-                  Tilaa omaan naapurustoosi
-                </button>
+                  class="w-full sm:w-auto font-display font-semibold text-lg bg-primary text-white rounded-full py-2 px-6"
+                >Tilaa omaan naapurustoosi</button>
               </div>
             </div>
             <div class="flex items-center justify-center">
@@ -60,25 +52,19 @@
       <section>
         <div class="container mx-auto">
           <div class="flex sm:min-h-screen items-center justify-center">
-            <div
-              class="w-full sm:w-3/5 bg-white shadow-xl rounded-lg px-16 py-6"
-            >
+            <div class="w-full sm:w-3/5 bg-white shadow-xl rounded-lg px-16 py-6">
               <div>
                 <h2
                   class="font-display font-extrabold text-2xl text-primary mb-4"
-                >
-                  Tilaa omaan taloyhtiöösi
-                </h2>
+                >Tilaa omaan taloyhtiöösi</h2>
               </div>
-              <form class="mt-8" action="#" method="POST">
+              <form class="mt-8" name="contact" netlify>
                 <label class="block mb-4">
-                  <span class="font-display font-semibold text-primary"
-                    >Nimesi</span
-                  >
+                  <span class="font-display font-semibold text-primary">Nimesi</span>
                   <input
                     aria-label="Nimesi"
                     name="name"
-                    type="name"
+                    type="text"
                     required
                     class="form-input mt-1 block w-full"
                     placeholder="Matti Meikäläinen"
@@ -86,13 +72,11 @@
                 </label>
 
                 <label class="block mb-4">
-                  <span class="font-display font-semibold text-primary"
-                    >Katuosoite</span
-                  >
+                  <span class="font-display font-semibold text-primary">Katuosoite</span>
                   <input
-                    aria-label="Nimesi"
-                    name="name"
-                    type="name"
+                    aria-label="Katuosoite"
+                    name="address"
+                    type="text"
                     required
                     class="form-input mt-1 block w-full"
                     placeholder="Mallikatu 1 a"
@@ -100,13 +84,11 @@
                 </label>
 
                 <label class="block mb-4">
-                  <span class="font-display font-semibold text-primary"
-                    >Postinumero</span
-                  >
+                  <span class="font-display font-semibold text-primary">Postinumero</span>
                   <input
-                    aria-label="Nimesi"
-                    name="name"
-                    type="name"
+                    aria-label="Postinumero"
+                    name="postnumber"
+                    type="text"
                     required
                     class="form-input mt-1 block w-full"
                     placeholder="01234"
@@ -114,13 +96,11 @@
                 </label>
 
                 <label class="block mb-4">
-                  <span class="font-display font-semibold text-primary"
-                    >Kaupunki</span
-                  >
+                  <span class="font-display font-semibold text-primary">Kaupunki</span>
                   <input
-                    aria-label="Nimesi"
-                    name="name"
-                    type="name"
+                    aria-label="Kaupunki"
+                    name="city"
+                    type="text"
                     required
                     class="form-input mt-1 block w-full"
                     placeholder="Kaupunki"
@@ -134,36 +114,34 @@
                   <div class="relative flex justify-center text-sm leading-5">
                     <span
                       class="px-2 bg-white font-display font-semibold text-primary"
-                      >Haluan laput kielellä</span
-                    >
+                    >Haluan laput kielellä</span>
                   </div>
                 </div>
                 <div class="flex flex-column items-center justify-center my-6">
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" checked />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="suomi" />
                       <span class="ml-2">Suomi</span>
                     </label>
                   </div>
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="svenska" />
                       <span class="ml-2">Svenska</span>
                     </label>
                   </div>
                   <div>
                     <label class="inline-flex items-center">
-                      <input type="checkbox" class="form-checkbox" />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="english" />
                       <span class="ml-2">English</span>
                     </label>
                   </div>
                 </div>
                 <div class="text-center">
                   <button
-                    class="font-display font-semibold text-lg bg-primary text-white rounded-full py-2 px-6 "
-                  >
-                    Lähetä lomake
-                  </button>
+                    class="font-display font-semibold text-lg bg-primary text-white rounded-full py-2 px-6"
+                    type="submit"
+                  >Lähetä lomake</button>
                 </div>
               </form>
             </div>
@@ -176,11 +154,7 @@
         <div class="pt-12 sm:pt-16 lg:pt-20">
           <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center">
-              <h2
-                class="font-display font-extrabold text-5xl text-primary mb-4"
-              >
-                Mistä on kyse?
-              </h2>
+              <h2 class="font-display font-extrabold text-5xl text-primary mb-4">Mistä on kyse?</h2>
               <p class="mt-4 font-display text-xl leading-7 text-gray-900">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
                 iaculis bibendum elit.
