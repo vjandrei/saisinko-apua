0f5152bb59c9ae0a95f454cc1600d2bbe0ecb9d0
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2020-03-22 09:25:39 +0200

    refactor

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -136,37 +136,19 @@
                 <div class="flex flex-column items-center justify-center my-6">
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        class="form-checkbox"
-                        name="launguage"
-                        value="suomi"
-                        v-model="formData.value"
-                      />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="suomi" />
                       <span class="ml-2">Suomi</span>
                     </label>
                   </div>
                   <div class="mr-3">
                     <label class="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        class="form-checkbox"
-                        name="launguage"
-                        value="svenska"
-                        v-model="formData.value"
-                      />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="svenska" />
                       <span class="ml-2">Svenska</span>
                     </label>
                   </div>
                   <div>
                     <label class="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        class="form-checkbox"
-                        name="launguage"
-                        value="english"
-                        v-model="formData.value"
-                      />
+                      <input type="checkbox" class="form-checkbox" name="launguage" value="english" />
                       <span class="ml-2">English</span>
                     </label>
                   </div>
@@ -183,6 +165,48 @@
         </div>
       </section>
     </div>
+    <section>
+      <div class="container mx-auto mb-24">
+        <div class="flex justify-center flex-wrap">
+          <div class="sm:block w-full md:flex-1 relative mb-16 md:mb-0">
+            <div class="w-full sm:w-1/2 relative z-30 px-2 xl:px-4 float-left">
+              <div
+                class="rounded-lg bg-white px-3 lg:px-6 xl:px-8 py-8 xl:py-16 text-center shadow-lg border border-gray-100 mt-32"
+              >
+                <img class="mx-auto w-full xl:w-48" src alt="Logo" />
+
+                <h4 class="text-gray-800 font-bold text-2xl mt-6">Grano</h4>
+                <p
+                  class="text-gray-600 mt-6 block md:hidden lg:block"
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem similique.</p>
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2 relative z-30 sm:pl-6 xl:pl-8 float-left">
+              <div
+                class="rounded-lg bg-white px-3 lg:px-6 xl:px-12 py-8 xl:py-16 text-center shadow-lg border border-gray-100"
+              >
+                <img class="mx-auto w-20 xl:w-32" src alt="Logo" />
+                <h4 class="text-gray-800 font-bold text-2xl mt-6">Terveystalo</h4>
+                <p
+                  class="text-gray-600 mt-6 block md:hidden lg:block"
+                >Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem similique.</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-1 flex flex-col justify-center z-30 ml-0 md:ml-12">
+            <h3
+              class="text-gray-800 font-bold text-xl sm:text-3xl lg:text-4xl xl:text-5xl pr-0 lg:pr-8 text-gray-800 font-bold ml-6 mr-6 sm:mr-0 leading-tight"
+            >Yhteystyökumppanit</h3>
+            <h4
+              class="ml-6 mt-4 sm:mt-8 mr-6 sm:mr-0 text-sm lg:text-xl xl:text-2xl font-hairline sm:font-medium lg:font-normal text-custom-purple"
+            >Lorem ipsum dolor sit amet, consectetur adipisicing elit. periores tempore, quos.</h4>
+            <p
+              class="ml-6 mt-4 sm:mt-8 mr-6 sm:mr-0 text-sm sm:text-lg xl:text-xl text-gray-600"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere illo deleniti sed ad hic. Corrupti fugiat ex eligendi quibusdam nemo sint saepe alias porro? Doloribus fuga laborum similique necessitatibus a.</p>
+          </div>
+        </div>
+      </div>
+    </section>
     <section>
       <div class="bg-gray-100">
         <div class="pt-12 sm:pt-16 lg:pt-20">
